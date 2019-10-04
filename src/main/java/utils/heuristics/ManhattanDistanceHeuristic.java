0d5369d925346d65ce9fd65fea13dbab67ef8677
ReCursia <saha2098@yandex.ru>
2019-10-04 23:43:44 +0300

    Update ManhattanDistanceHeuristic.java

--- src/main/java/utils/heuristics/ManhattanDistanceHeuristic.java
+++ src/main/java/utils/heuristics/ManhattanDistanceHeuristic.java
@@ -9,10 +9,6 @@ public class ManhattanDistanceHeuristic implements Heuristical<Table> {
         int distanceCounter = 0;
         int[][] stateCells = state.getCells();
         int[][] finalStateCells = finalState.getCells();
-        for (int i = 0; i < stateCells.length; i++) {
-            for (int j = 0; j < stateCells[i].length; j++) {
-                for (int k = 0; k < stateCells.length; k++) {
-                    for (int l = 0; l < stateCells[k].length; l++) {
                         if (stateCells[i][j] == finalStateCells[k][l]) {
                             distanceCounter += Math.abs(k - i) + Math.abs(l - j);
                         }
